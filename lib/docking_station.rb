afc65bbcdf07d315cb550db0ce748e5276b9d153
your name <akram.rasikh@gmail.com>
2016-09-29 12:35:24 +0100

    private methods & single respon

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,12 +6,21 @@ class DockingStation
   end
 
   def release_bike
-    fail "No more bikes you fool!" if @bikes.count == 0
+    fail "No more bikes you fool!" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail "No more spaces left" if @bikes.count == 20
+    fail "No more spaces left" if full?
     @bikes << bike
+  end
+
+  private
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.count >= 0
   end
 end
