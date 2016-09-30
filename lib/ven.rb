25d6914bed2ba01b920a639906624a797ea3c56e
Rosie <r.gollancz@gmail.com>
2016-09-30 15:59:36 +0100

    Fix take bikes method in ven and added passing test

--- lib/ven.rb
+++ lib/ven.rb
@@ -1,7 +1,7 @@
 class Ven
    def take_bikes(location)
      if location.class == DockingStation
-       location.bikes.collect{|bike| bike.working == false}
+       location.bikes.select{|bike| bike unless bike.working}
      elsif location.class == Garage
      else
        fail "Ven doesn't go there"
--- spec/ven_spec.rb
+++ spec/ven_spec.rb
@@ -2,21 +2,22 @@ require './lib/ven'
 describe Ven do
   let(:station) {DockingStation.new}
   let(:garage) {double()}
-  let(:working_bike) {double(:working => true)}
-  let(:broken_bike) {Bike.new}
+  let(:broken_bike) {double("bike", :working => false)}
+  let(:working_bike) {double("bike", :working => true)}
 
   it 'takes bikes' do
     expect(subject).to respond_to(:take_bikes).with(1).argument
   end
 
   it 'takes a broken bike from a station', focus: true do
-    broken_bike.report_broken
+    station.dock(working_bike)
     station.dock(broken_bike)
-    expect(false).to be false
+    station.dock(broken_bike)
+    expect(subject.take_bikes(station)[0].working).to be false
   end
 
   it 'takes a working bike from a garage' do
-    expect(subject.take_bikes(garage)[0].working).to be_true
+    expect(subject.take_bikes(garage)[0].working).to be true
   end
 
 end
